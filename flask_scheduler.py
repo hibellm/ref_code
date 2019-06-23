39e644b147a6ba1778a3da8e1604edfb03e3cbd9
Marcus <marcus.hibell@roche.com>
2019-06-23 11:07:58 +0200

    example flask scheduling a job

---
+++ flask_scheduler.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Demonstrating APScheduler feature for small Flask App. """
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask
+import pickle
+import datetime
+
+
+def sensor():
+    """ Function for test purposes. """
+    sensor=f"Scheduler is alive! {datetime.datetime.now()}"
+    pickle.dump(sensor, open("save.sensor", "wb"))
+
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(sensor, 'interval', seconds=60)
+sched.start()
+
+app = Flask(__name__)
+
+
+@app.route("/home")
+def home():
+    sensor=""
+    try:
+        sensor=pickle.load(open("save.sensor", "rb"))
+    except:
+        pass
+
+    return f"Welcome Home :) ! {sensor}"
+
+
+if __name__ == "__main__":
+    app.run()
